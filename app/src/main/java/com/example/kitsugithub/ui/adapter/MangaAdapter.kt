9c9386912f870faa3fbdd1079d45f7e7619033b8
RenHeyzer <vesolyyradin@gmail.com>
2023-05-16 19:52:36 +0600

    добавил нов MangaAdapter

---
+++ app/src/main/java/com/example/kitsugithub/ui/adapter/MangaAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.kitsugithub.ui.adapter
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kitsugithub.Models.DataItem
+import com.example.kitsugithub.databinding.ItemKitsuBinding
+import com.example.kitsugithub.extension.setImage
+
+class MangaAdapter: PagingDataAdapter<DataItem, MangaAdapter.ViewHolder>(
+    AnimeDiffCallBack
+) {
+
+    inner class ViewHolder(private val binding: ItemKitsuBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun onBind(item: DataItem) {
+            binding.itemText.text = item.attributes.titles.enJp
+            binding.itemImage.setImage(item.attributes.posterImage.original)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemKitsuBinding.inflate(
+                LayoutInflater.from(
+                    parent.context
+                ), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        getItem(position)?.let { holder.onBind(it) }
+    }
+
+    object AnimeDiffCallBack : DiffUtil.ItemCallback<DataItem>() {
+        override fun areItemsTheSame(
+            oldItem: DataItem,
+            newItem: DataItem,
+        ): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(
+            oldItem: DataItem,
+            newItem: DataItem,
+        ): Boolean {
+            return oldItem.id == newItem.id
+        }
+    }
+}
